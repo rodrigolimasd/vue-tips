eeca183b84a0b1cc659dab2551babf8a288a7c42
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-08-03 09:15:11 -0300

    vue component advanced

---
+++ components/quotes/src/components/Quote.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="quote">
+        {{ text }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: String
+    }
+
+}
+</script>
+
+<style scoped>
+    .quote {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
