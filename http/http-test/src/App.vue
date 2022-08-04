f0093b0cbd87009f47d1e6889db389fc58fdc13c
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-08-04 10:40:41 -0300

    vue form, axios post and get

--- http/http-test/src/App.vue
+++ http/http-test/src/App.vue
@@ -1,11 +1,58 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP with Axios</h1>
+		<b-card>
+			<b-form-group label="Name: ">
+				<b-form-input type="text" size="lg"
+					v-model="user.name"
+					placeholder="Name">
+				</b-form-input>
+			</b-form-group>
+			<b-form-group label="E-mail: ">
+				<b-form-input type="email" size="lg"
+					v-model="user.email"
+					placeholder="E-mail">
+				</b-form-input>
+			</b-form-group>
+			<hr>
+			<b-button @click="save" variant="primary" size="lg">Save</b-button>
+			<b-button @click.prevent="getUsers" variant="success" size="lg" class="ml-2">Get Users</b-button>
+		</b-card>
+		<hr>
+		<b-list-group>
+			<b-list-group-item v-for="(u, id) in users" :key="id">
+				<strong>Name: </strong> {{ u.name}}<br>
+				<strong>Email: </strong> {{ u.email}}<br>
+				<strong>ID: </strong> {{ id }}<br>
+			</b-list-group-item>
+		</b-list-group>
 	</div>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			users: [],
+			user: {
+				name: '',
+				email: ''
+			}
+		}
+	},
+	methods: {
+		save() {
+			this.$http.post('user.json', this.user)
+				.then(res => {
+						this.user.name = ''
+						this.user.email = ''
+					})
+		},
+		getUsers() {
+			this.$http.get('user.json')
+				.then(res => this.users = res.data)
+		}
+	},
 	created() {
 		// this.$http.post('user.json',{
 		// 	name: 'Rodrigo',
--- http/http-test/src/plugins/axios.js
+++ http/http-test/src/plugins/axios.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://vueapp-ad652-default-rtdb.firebaseio.com/'
+//axios.defaults.baseURL = 'https://vueapp-ad652-default-rtdb.firebaseio.com/'
 
 Vue.use({
     install(Vue){
-        Vue.prototype.$http = axios
+        //Vue.prototype.$http = axios
+        Vue.prototype.$http = axios.create({
+            baseURL: 'https://vueapp-ad652-default-rtdb.firebaseio.com/'
+        })
     }
 })
