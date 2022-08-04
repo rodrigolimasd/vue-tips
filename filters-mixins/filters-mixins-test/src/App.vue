915d76f2a77aef1fef585249c2f2b678835f57e7
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-08-03 21:24:57 -0300

    vue basic filter

---
+++ filters-mixins/filters-mixins-test/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="app">
+		<h1>Filter & Mixins</h1>
+		<hr>
+		<p>{{ cpf | cpf | inverse}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	filters: {
+		cpf(value) {
+			const arr = value.split('')
+			arr.splice(3, 0, '.')
+			arr.splice(7, 0, '.')
+			arr.splice(11, 0, '-')
+			return arr.join('')
+		}
+	},
+	data() {
+		return {
+			cpf: '92179437030'
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
