37baacc3a43fb8ae3e75034e5f6f4f4f6c3d9e0c
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-08-09 17:22:03 -0300

    multi router view(named)

--- routes/routes-test/src/App.vue
+++ routes/routes-test/src/App.vue
@@ -1,16 +1,18 @@
 <template>
 	<div id="app">
 		<h1>Routes with VueRouter</h1>
-		<Menu />
+		<!-- <Menu /> -->
+		<router-view name="menu"></router-view>
 		<router-view />
+		<router-view name="menuBottom"></router-view>
 	</div>
 </template>
 
 <script>
-import Menu from './components/template/Menu'
+//import Menu from './components/template/Menu'
 
 export default {
-	components: { Menu }
+	//components: { Menu }
 }
 </script>
 
--- routes/routes-test/src/router.js
+++ routes/routes-test/src/router.js
@@ -5,6 +5,8 @@ import User from './components/user/User'
 import UserList from './components/user/UserList'
 import UserDetails from './components/user/UserDetails'
 import UserEdit from './components/user/UserEdit'
+import Menu from './components/template/Menu'
+import MenuPt from './components/template/MenuPt'
 
 Vue.use(Router)
 
@@ -13,12 +15,21 @@ export default new Router({
     routes:[
     {
         path: '/',
-        component: Start,
-        name: 'homeRoute'
+       // component: Start,
+        name: 'homeRoute',
+        components: {
+            default: Start,
+            menu: Menu
+        }
     },
     {
         path: '/user',
-        component: User,
+        //component: User,
+        components: {
+            default: User,
+            menu: MenuPt,
+            menuBottom: MenuPt
+        },
         props: true,
         children: [
             { path: '', component: UserList },
